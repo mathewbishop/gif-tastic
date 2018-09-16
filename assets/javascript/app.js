9623e5bb8ae5f5059def903c47e1d64d78306fc3
Mathew Bishop <39043451+MattyyB@users.noreply.github.com>
2018-09-16 16:02:22 -0500

    api query working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,32 @@
 
 
-var API_KEY = "0Q6mzyUdggwOoNd7SEBsuy19WrVSW06y&limit=10"
-var queryURL = "https://api.giphy.com/v1/gifs/search?q="
+
 var dogArry = ["corgi", "mastiff", "pitbull", "terrier", "beagle"];
 
-    for (var i = 0; i < dogArry.length; i++) {
+for (var i = 0; i < dogArry.length; i++) {
     var dogButton = $("<button>");
         dogButton.attr("data-dog", dogArry[i]);
         dogButton.text(dogArry[i]);
-        $('body').append(dogButton);
+        $('body').append(dogButton);        
+}
+
+
+$('button').click(function() {
+    var dog = $(this).attr("data-dog");
+    // var API_KEY = "0Q6mzyUdggwOoNd7SEBsuy19WrVSW06y&limit=10"
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + dog + "&api_key=0Q6mzyUdggwOoNd7SEBsuy19WrVSW06y&limit=10";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+    
+    .then(function(response) {
+        console.log(queryURL);
+        console.log(response);
         
-    }
+        
+    })
+
+
+})
